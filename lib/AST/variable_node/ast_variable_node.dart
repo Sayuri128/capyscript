f1652b1b8ce120189587341ed022ddbd55bb8a0d
armatura24 <armaturapengva2222@gmail.com>
2023-12-10 22:11:38 +0200

    serialize ast_variable_node.dart

--- lib/AST/variable_node/ast_variable_node.dart
+++ lib/AST/variable_node/ast_variable_node.dart
@@ -1,3 +1,4 @@
+import 'package:json_annotation/json_annotation.dart';
 /*
  * Copyright (c) 2023 armatura24
  * All right reserved
@@ -6,7 +7,15 @@
 import 'package:capyscript/AST/ast_node.dart';
 import 'package:capyscript/AST/function_declaration/ast_funcation_declaration_node.dart';
 
+part 'ast_variable_node.g.dart';
+
+@JsonSerializable(explicitToJson: true)
 class ASTVariableNode extends ASTNode {
+  factory ASTVariableNode.fromJson(Map<String, dynamic> json) =>
+      _$ASTVariableNodeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ASTVariableNodeToJson(this);
+
   final String functionName;
   final String variableName;
 
